983f16f67facff3d74fd6502f376908e96fd138d
collinkatz <collin.katz@gmail.com>
2024-08-14 10:17:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# cliPomodoro
+## About
+- cliPomodoro is an easy to use, minimal setup, pomodoro timer.
+- This timer was created to help with eye strain, and follows the 20-20-20 rule. I.e. if you spend 20 minutes looking at a screen, look at something 20 feet away for 20 seconds.
+- Other pomodoro applications force you to pay money to customize the timer, but this allows you to customize your timer, and works seamlessly on your work, school, or home computer (as long as you have python installed).
+
+## How to use
+### Requirements
+- Have git installed
+- Have python and a package manager of your choice installed (pip works fine)
---
+++ timer.py
@@ -0,0 +1,63 @@
+import os
+import time
+from plyer import notification
+
+wait_sequence = [20*60, 30]
+
+def seconds_to_timer(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    return f"{int(minutes):02d}:{int(seconds):02d}"
+
+
+def print_7_seg_time(time_str):
+    dict = {
+        "0":('▓▓▓','▓ ▓','▓ ▓','▓ ▓','▓▓▓'),
+        "1":('  ▓','  ▓','  ▓','  ▓','  ▓'),
+        "2":('▓▓▓','  ▓','▓▓▓','▓  ','▓▓▓'),
+        "3":('▓▓▓','  ▓','▓▓▓','  ▓','▓▓▓'),
+        "4":('▓ ▓','▓ ▓','▓▓▓','  ▓','  ▓'),
+        "5":('▓▓▓','▓  ','▓▓▓','  ▓','▓▓▓'),
+        "6":('▓▓▓','▓  ','▓▓▓','▓ ▓','▓▓▓'),
+        "7":('▓▓▓','  ▓','  ▓','  ▓','  ▓'),
+        "8":('▓▓▓','▓ ▓','▓▓▓','▓ ▓','▓▓▓'),
+        "9":('▓▓▓','▓ ▓','▓▓▓','  ▓','▓▓▓'),
+        ":":('   ',' ▓ ','   ',' ▓ ','   '),
+    }
+
+    time_list = list(time_str)
+    for seg in range(5):
+        print(' '.join([dict.get(value)[seg] for value in time_list]))
+    print("Pomodoro Timer...")
+    print()
+
+
+def print_time_up():
+    print()
+    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+    print("▓▓▓▓▓▓▓▓TIME▓▓▓▓▓▓▓▓")
+    print("▓▓▓▓▓▓▓▓▓UP▓▓▓▓▓▓▓▓▓")
+    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+    print()
+    notification.notify(  
+        title = "Pomodoro",  
+        message = "Time is up...",  
+        app_icon = None,  
+        timeout = 10,  
+        toast = False  
+    )  
+
+try:
+    # Shrink the CLI to just fit the timer so it doesn't take up so much space
+    os.system('mode con: cols=20 lines=9')
+    
+    while True:
+        for wait_time in wait_sequence:
+            for i in range(wait_time):
+                time.sleep(1)
+                print_7_seg_time(seconds_to_timer(wait_time - i))
+            print_time_up()
+            time.sleep(5)
+except KeyboardInterrupt:
+    print("Pomodoro Timer Stopped...")
